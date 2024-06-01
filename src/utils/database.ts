0e5a3761e22af555dc260e9d1c8bce862cdb7df1
Jamiu Makanjuola <mcbroh@msn.com>
2024-06-01 20:40:26 +0200

    Added postgresss details

--- src/utils/database.ts
+++ src/utils/database.ts
@@ -1,12 +1,13 @@
 // lib/initDb.ts
-import { Pool } from 'pg';
+import { Pool } from "pg";
 
 const pool = new Pool({
   user: process.env.POSTGRES_USER,
   host: process.env.POSTGRES_HOST,
   database: process.env.POSTGRES_DATABASE,
   password: process.env.POSTGRES_PASSWORD,
-  port: parseInt(process.env.POSTGRES_PORT || '5432', 10),
+  port: parseInt(process.env.POSTGRES_PORT || "5432", 10),
+  ssl: true,
 });
 
 const initDb = async () => {
