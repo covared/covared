768a535a298757e676dc1e28cb60cd96a8323540
raylite <qasimbabatunde@gmail.com>
2024-07-16 23:56:02 +0100

    Updated home page content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,48 @@ import NavBar from "@/components/NavBar";
 export default function index() {
   return (
     <div className="container">
-    <div className="row justify-content-center">
-      <div className="col-10">
-        <NavBar />
-  
-    <h1>Landing Page</h1>
-  </div>
-  </div>
-  </div>
+      <div className="row justify-content-center">
+        <div className="col-10">
+          <NavBar />
+        </div>
+
+        <div>
+        <h1>Welcome to Springverse</h1>
+        <div className="text-block"></div>
+          <h2>
+            Springsverse is a dynamic way to engage more students in science and maths.
+          </h2>
+        </div>
+        <div className="button-block">
+          <a href="https://example.com">
+            <button>Join Springsverse pilot waitlist</button>
+          </a>
+        </div>
+        <div className="buttom-text">
+          <p>
+            Springsverse AI-powered platform offers you new, innovative tools to engage all your students in science 
+            and maths and ready-to-go resources to allow your teachers to do their job to their optimal.
+          </p>
+          <p>
+            With Springsverse, you are going to discover:<b />
+            New, simple tools that would engage and improve all your students' outcome in science and maths.
+          </p>
+          <p>
+            Ready-to-go resources to build their skills in using science and maths.
+          </p>
+          <p>
+            Topping that is connecting your school to employers with our <strong>exclusive</strong> live streaming of scientists, 
+            mathematicians and professionals doing science and maths in their job to tell your students their stories 
+            and to allow your students to see how they use science and maths in the real world.
+          </p>
+          <p>
+            <strong>Join</strong> Springsverse pilot waiting list and start steering your school with new, dynamic, 
+            innovative tools and ready-to-go resources to build your future scientists and mathematicians.
+          </p>
+        </div>
+      
+      </div>
+    
+    </div>
   )
 }
