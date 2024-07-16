6b1f424162a18cc186280525fccfa0a82f999397
raylite <qasimbabatunde@gmail.com>
2024-07-16 19:27:15 +0100

    Imported navbar to landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,15 @@
+import NavBar from "@/components/NavBar";
 
 export default function index() {
   return (
-  <div>
+    <div className="container">
+    <div className="row justify-content-center">
+      <div className="col-10">
+        <NavBar />
+  
     <h1>Landing Page</h1>
+  </div>
+  </div>
   </div>
   )
 }
