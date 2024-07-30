7c106b047d5e559a8b299db319051d7161eab5b3
raylite <qasimbabatunde@gmail.com>
2024-07-30 18:14:40 +0100

    Nav bar to primary

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -27,7 +27,7 @@ const NavBar: React.FC = () => {
   
 
   return (
-    <Navbar collapseOnSelect bg="light" data-bs-theme="light" expand="lg" className="bg-body-tertiary justify-content-between">
+    <Navbar collapseOnSelect bg="primary" data-bs-theme="primary" expand="lg" className="bg-body-tertiary justify-content-between">
       <Container>
         <Navbar.Brand href="/">
             Springverse
